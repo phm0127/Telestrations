bb6f14849a08604d6d7a584c2748c6fdd65a3244
1000min <o_ogog@naver.com>
2020-06-03 00:16:36 +0900

    프로젝트 시작

---
+++ Telestrations/src/main/java/com/ssafybus/telestrations/TelestrationsApplication.java
@@ -0,0 +1,13 @@
+package com.ssafybus.telestrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelestrationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelestrationsApplication.class, args);
+	}
+
+}
