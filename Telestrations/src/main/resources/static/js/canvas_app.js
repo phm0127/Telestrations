b83a7582758263c8a8fc5130b603d1a1eca61264
1000min <o_ogog@naver.com>
2020-06-03 02:00:25 +0900

    캔버스쪽 기능 추가

--- Telestrations/src/main/resources/static/js/canvas_app.js
+++ Telestrations/src/main/resources/static/js/canvas_app.js
@@ -3,7 +3,11 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor")
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const notify = document.getElementById("modnotify");
 const saveBtn = document.getElementById("jsSave");
+const undoBtn = document.getElementById("jsUndo");
+const redoBtn = document.getElementById("jsRedo");
+const eraseBtn = document.getElementById("jsErase");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 500;
@@ -19,9 +23,49 @@ ctx.lineWidth = 2.5;
 
 let painting = false;
 let filling = false;
+let tempImage=null;
+
+/*
+ */
+let cPushArray = new Array();
+let cStep = 0;
+cPushArray.push(document.getElementById('jsCanvas').toDataURL());
+
+function canvasPush() {
+	console.log("push");
+    cStep++;
+    if (cStep < cPushArray.length) { cPushArray.length = cStep; }
+    cPushArray.push(document.getElementById('jsCanvas').toDataURL());
+}
+
+function handleUndoClick(event) {
+	console.log("redo");
+    if (cStep > 0) {
+        cStep--;
+        var canvasPic = new Image();
+        canvasPic.src = cPushArray[cStep];
+        canvasPic.onload = function () { ctx.drawImage(canvasPic, 0, 0); }
+    }
+}
+
+function handleRedoClick(event) {
+    if (cStep < cPushArray.length-1) {
+        cStep++;
+        var canvasPic = new Image();
+        canvasPic.src = cPushArray[cStep];
+        canvasPic.onload = function () { ctx.drawImage(canvasPic, 0, 0); }
+    }
+}
+/*
+ */
 
 function stopPainting() {
     painting = false;
+    canvasPush();
+}
+function stopPainting2() {
+	painting = false;
+	
 }
 
 function startPainting() {
@@ -35,10 +79,16 @@ function onMouseMove(event) {
         ctx.beginPath();
         ctx.moveTo(x, y);
     } else {
-        ctx.lineTo(x, y);
+    	if(!filling){
+    		ctx.lineTo(x, y);
+    	}else{
+    		ctx.moveTo(x, y);
+    	}
         ctx.stroke();
     }
 }
+
+
 
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
@@ -55,9 +105,11 @@ function handleModeClick() {
     if (filling === true) {
         filling = false;
         mode.innerText = "채우기";
+        notify.innerText = "그리기 모드";
     } else {
         filling = true;
         mode.innerText = "그리기";
+        notify.innerText = "채우기 모드";
     }
 }
 
@@ -78,13 +130,29 @@ function handleSaveClick(event) {
     
     link.click();
     console.log(link);
+    
 }
+function handleEraseClick(event) {
+	ctx.fillStyle = "white";
+	ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+	ctx.strokeStyle = INITIAL_COLOR;
+	ctx.fillStyle = INITIAL_COLOR;
+	ctx.lineWidth = 2.5;
+	filling = false;
+    mode.innerText = "채우기";
+    notify.innerText = "그리기 모드";
+    canvasPush();
+	
+}
+
+
+
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
-    canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting2);
     canvas.addEventListener("click", handleCanvasClick);
     canvas.addEventListener("contextmenu", handleCM);
 }
@@ -101,4 +169,13 @@ if (mode) {
 
 if (saveBtn) {
     saveBtn.addEventListener("click", handleSaveClick);
+}
+if (eraseBtn) {
+	eraseBtn.addEventListener("click", handleEraseClick);
+}
+if (undoBtn) {
+	undoBtn.addEventListener("click", handleUndoClick);
+}
+if (redoBtn) {
+	redoBtn.addEventListener("click", handleRedoClick);
 }
--- Telestrations/src/main/webapp/views/paint/canvas.jsp
+++ Telestrations/src/main/webapp/views/paint/canvas.jsp
@@ -13,6 +13,7 @@
 	<p style="font-size: 2rem; margin : 2rem auto; letter-spacing: 3px;">SSAFY TELESTRATIONS</p>
 
     <canvas id="jsCanvas" class="canvas"></canvas>
+    <p id="modnotify" style="font-size: 1rem; margin-top : 2rem; letter-spacing: 1px; font-weight:bold; font-weight: 800;">그리기 모드</p>
     <div class="controls">
         <div class="controls_range">
             <input type="range" id="jsRange" min="0.1" max="5.0" value="2.5" step="0.1" />
@@ -20,6 +21,9 @@
         <div class=" controls_btns ">
             <button id="jsMode">채우기</button>
             <button id="jsSave">제출</button>
+            <button id="jsErase">전체지우기</button>
+            <button id="jsUndo"><img src="/img/undo.png" style="height: 10px;" alt="되돌리기"></button>
+            <button id="jsRedo"><img src="/img/redo.png" style="height: 10px;" alt="앞돌리기"></button>
         </div>
         <div class="controls_colors " id="jsColors ">
             <div class="controls_color jsColor" style="background-color: #2c2c2c "></div>
